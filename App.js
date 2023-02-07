12b61d1b43df1c99b26bee94cf0f68df2ef787ba
Deniz <denizerdem@livewallgroup.com>
2023-02-07 14:46:37 +0100

    demo done

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, FlatList, TouchableOpacity, Button, TextInput, StyleSheet } from 'react-native';
+import NewsItem from './components/NewsItem';
 
 export default function App() {
+  const [news, setNews] = useState([]);
+  const [newsData, setNewsData] = useState({ title: '', content: '' });
+
+  useEffect(() => {
+
+    fetch('http://192.168.2.11:8000/api/news')
+      .then(response => response.json())
+      .then(data => {
+        setNews(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+  }, []);
+
+  function handleAdd() {
+    setNews(
+      [...news, { id: news.length + 2, title: newsData.title, content: newsData.content }]
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text>React native demo</Text>
+
+      <FlatList
+        data={news}
+        renderItem={({ item }) => <NewsItem title={item.title} content={item.content} category={item.category.name} />}
+        keyExtractor={item => item.id}
+      />
+
+      <View>
+        <TextInput style={styles.textInput} placeholder="Title" onChangeText={text => setNewsData({ ...newsData, title: text })} />
+        <TextInput style={styles.textInput} placeholder="Content" onChangeText={text => setNewsData({ ...newsData, content: text })} />
+
+        <View style={styles.buttonGroup}>
+          <Button title="Add" onPress={handleAdd} />
+
+          <TouchableOpacity onPress={handleAdd}>
+            <Text style={{ color: "red" }}>Voeg toe</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
   },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    margin: 10,
+  }
 });
+
+
+
+
